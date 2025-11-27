79291d095726204c30d147141f7be743dd43dfba
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-11-27 22:28:24 +0900

    implemented acceser

--- packages/frameworks/rmf-core/src/frame.rs
+++ packages/frameworks/rmf-core/src/frame.rs
@@ -3,4 +3,6 @@ use crate::*;
 pub trait Frame {
     type Image: image::Image;
     type Audio: audio::Audio;
+    fn image(&self) -> Option<&Self::Image>;
+    fn audio(&self) -> Option<&Self::Audio>;
 }
--- packages/frameworks/rmf-static/src/frame.rs
+++ packages/frameworks/rmf-static/src/frame.rs
@@ -2,10 +2,19 @@ use rmf_macros::delegate_implements;
 
 use crate::{audio::Audio, image::Image};
 
-pub struct Frame {}
+pub struct Frame {
+    image: Option<Image>,
+    audio: Option<Audio>,
+}
 
 #[delegate_implements]
 impl rmf_core::Frame for Frame {
     type Image = Image;
     type Audio = Audio;
+    fn image(&self) -> Option<&Image> {
+        self.image.as_ref()
+    }
+    fn audio(&self) -> Option<&Audio> {
+        self.audio.as_ref()
+    }
 }
