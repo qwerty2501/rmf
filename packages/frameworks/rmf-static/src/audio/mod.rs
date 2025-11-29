b148ab9f5916c7b01c043cf28fab957b089f02b6
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-11-30 01:30:26 +0900

    rename filename

--- packages/frameworks/rmf-static/src/audio/mod.rs
+++ packages/frameworks/rmf-static/src/audio/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "use_ffmpeg")]
-pub mod ffmpeg;
+pub mod ffmpeg_audio;
 mod utils;
 
 #[cfg(feature = "ffmpeg_audio_as_default")]
-pub use ffmpeg::*;
+pub use ffmpeg_audio::*;
 pub use utils::*;
