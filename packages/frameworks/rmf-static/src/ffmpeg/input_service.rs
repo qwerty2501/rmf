a42a8279aae09a2d16380d99ed7e89ce73333f21
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-12-06 21:39:44 +0900

    implemented AudioInput

--- packages/frameworks/rmf-static/src/ffmpeg/input_service.rs
+++ packages/frameworks/rmf-static/src/ffmpeg/input_service.rs
@@ -1,45 +1,63 @@
-use std::{
-    ffi::CString,
-    os::unix::ffi::OsStrExt,
-    path::Path,
-};
+use std::{ffi::CString, os::unix::ffi::OsStrExt, path::Path};
 
-use rmf_core::{Error, InputSource, Result, image::ImageInputService};
+use rmf_core::{Error, InputSource, Result, audio::AudioInputService, image::ImageInputService};
+use rmf_macros::delegate_implements;
 use rsmpeg::avformat::AVFormatContextInput;
 
 use crate::{
-    Image,
-    ffmpeg::AVFormatImageContentCursor,
+    Audio, Image,
+    ffmpeg::{AVFormatAudioContentCursor, AVFormatImageContentCursor},
 };
 
 pub struct AVFormatImageInputService {
     source: InputSource,
 }
 
+pub struct AVFormatAudioInputService {
+    source: InputSource,
+}
+
+#[delegate_implements]
 impl ImageInputService for AVFormatImageInputService {
     type Item = Image;
     type ContentCursor = AVFormatImageContentCursor;
     fn cursor(&self) -> Result<AVFormatImageContentCursor> {
-        AVFormatImageContentCursor::try_new(self.make_input()?)
+        AVFormatImageContentCursor::try_new(make_input(&self.source)?)
     }
 }
 
-impl AVFormatImageInputService {
-    fn try_from_path_input(path: impl AsRef<Path>) -> Result<AVFormatContextInput> {
-        let path = path.as_ref();
-        AVFormatContextInput::open(&CString::new(path.as_os_str().as_bytes().to_vec()).unwrap())
-            .map_err(|e| Error::new_input(e.into()))
-    }
-    fn make_input(&self) -> Result<AVFormatContextInput> {
-        match &self.source {
-            InputSource::Path(path) => Self::try_from_path_input(path),
-        }
+#[delegate_implements]
+impl AudioInputService for AVFormatAudioInputService {
+    type Item = Audio;
+    type ContentCursor = AVFormatAudioContentCursor;
+    fn cursor(&self) -> Result<AVFormatAudioContentCursor> {
+        AVFormatAudioContentCursor::try_new(make_input(&self.source)?)
     }
 }
 
+#[delegate_implements]
 impl rmf_core::image::ImageInputServiceProvider for AVFormatImageInputService {
     type InputService = Self;
     fn try_new(source: InputSource) -> Result<Self> {
         Ok(Self { source })
+    }
+}
+
+#[delegate_implements]
+impl rmf_core::audio::AudioInputServiceProvider for AVFormatAudioInputService {
+    type InputService = Self;
+    fn try_new(source: InputSource) -> Result<Self> {
+        Ok(Self { source })
+    }
+}
+
+fn try_from_path_input(path: impl AsRef<Path>) -> Result<AVFormatContextInput> {
+    let path = path.as_ref();
+    AVFormatContextInput::open(&CString::new(path.as_os_str().as_bytes().to_vec()).unwrap())
+        .map_err(|e| Error::new_input(e.into()))
+}
+fn make_input(source: &InputSource) -> Result<AVFormatContextInput> {
+    match source {
+        InputSource::Path(path) => try_from_path_input(path),
     }
 }
