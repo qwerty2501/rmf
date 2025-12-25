26aeba097b834f99f98fd09f78b5abf456d447a2
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-12-25 20:48:38 +0900

    move file

--- packages/frameworks/rmf-static/src/audio/mod.rs
+++ packages/frameworks/rmf-static/src/audio/mod.rs
@@ -1,9 +1,9 @@
+mod default_input;
 #[cfg(feature = "use_ffmpeg")]
 pub mod ffmpeg_audio;
-mod input_service;
 mod utils;
 
+pub use default_input::*;
 #[cfg(feature = "ffmpeg_audio_as_default")]
 pub use ffmpeg_audio::*;
-pub use input_service::*;
 pub use utils::*;
