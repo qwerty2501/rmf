46e61ddeb68c20cb4caf7297d228f0d7c6cfd0fe
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2026-02-25 17:01:02 +0900

    wip

--- examples/simple-video-editor/src/audio_app.rs
+++ examples/simple-video-editor/src/audio_app.rs
@@ -1,6 +1,12 @@
 use derive_new::new;
-use rmf_host::InputSource;
-use std::path::PathBuf;
+use rmf_host::{
+    Content, InputSource, Timestamp,
+    audio::{self, Audio, AudioInputService},
+    service::{ContentCursorTrait, ContentStreamServiceTrait},
+};
+use std::{collections::VecDeque, path::PathBuf, sync::Arc, time::Duration};
+use tokio::sync::mpsc::{self, Receiver};
+use tokio::time::{Instant, sleep_until};
 
 #[derive(new)]
 pub struct AudioPlayer {
@@ -13,13 +19,72 @@ impl AudioPlayer {
     }
 }
 
-async fn decode_audio_loop(path: PathBuf) {
-    if let Err(err) = inner_decode_loop(path).await {
+async fn decode_audio_loop(path: PathBuf, sender: mpsc::Sender<Message>) {
+    if let Err(err) = inner_decode_loop(path, sender).await {
         eprintln!("{err}");
     }
 }
-async fn inner_decode_loop(path: PathBuf) -> anyhow::Result<()> {
+
+#[derive(Clone)]
+pub enum Message {
+    Received(Arc<Audio>),
+}
+
+struct InnerContent {
+    offset: Timestamp,
+    audio: Audio,
+}
+
+impl InnerContent {
+    fn from_rmf_content(content: Content<Audio>) -> Self {
+        Self {
+            offset: content.offset(),
+            audio: content.item().clone(),
+        }
+    }
+}
+
+async fn inner_decode_loop(path: PathBuf, sender: mpsc::Sender<Message>) -> anyhow::Result<()> {
     let input_source = InputSource::new_path(path.clone());
+    let input_service = AudioInputService::try_new(input_source)?;
+    let mut cursor = input_service.cursor()?;
+    const MAX_QUEUE_SIZE: usize = 10;
+    let mut content_queue = VecDeque::<InnerContent>::with_capacity(MAX_QUEUE_SIZE);
+
+    let mut start_instant: Option<Instant> = None;
+    let mut start_offset: Option<Timestamp> = None;
+
+    loop {
+        while content_queue.len() < MAX_QUEUE_SIZE {
+            if let Some(content) = cursor.read()? {
+                content_queue.push_back(InnerContent::from_rmf_content(content));
+            } else {
+                break;
+            }
+        }
+
+        if let Some(content) = content_queue.pop_front() {
+            if start_instant.is_none() {
+                start_instant = Some(Instant::now());
+                start_offset = Some(content.offset);
+            }
+            let base_start = start_instant.unwrap();
+            let base_offset = start_offset.unwrap();
+
+            let audio_elapsed = content.offset - base_offset;
+
+            let audio_elapsed_duration =
+                Duration::from_millis(audio_elapsed.as_milliseconds() as u64);
+
+            let target_time = base_start + audio_elapsed_duration;
+
+            sleep_until(target_time).await;
+            let a = Arc::new(content.audio);
+            sender.send(Message::Received(a)).await?;
+        } else {
+            break;
+        }
+    }
 
     Ok(())
 }
